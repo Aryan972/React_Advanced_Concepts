952641c209e7a2c59a81e6ae11212029e84f9467
Aryan972 <32037736+Aryan972@users.noreply.github.com>
2026-03-06 16:35:17 +0530

    feat: Designed the Basic Router components
    1. BrowserRouter
    2. Routes
    3. Route
    4. Link

---
+++ react_routers/react-router/src/App.jsx
@@ -0,0 +1,20 @@
+import {Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/About' element={<About />}></Route>
+      </Routes>
+    </>
+    
+  )
+}
+
+export default App;
