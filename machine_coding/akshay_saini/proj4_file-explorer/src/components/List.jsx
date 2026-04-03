70a8d08b21bf8b32d7dd8fea160003a1babd4280
Aryan972 <32037736+Aryan972@users.noreply.github.com>
2026-04-03 21:10:45 +0530

    feat: Added the remove file functionality
    1. delete folder
    2. Creating idx dynamically using Date.now() function.

--- machine_coding/akshay_saini/proj4_file-explorer/src/components/List.jsx
+++ machine_coding/akshay_saini/proj4_file-explorer/src/components/List.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function List( {list, addNodeToList} ) {
+export default function List( {list, addNodeToList, deleteNode} ) {
 
     const [isExpanded, setIsExpanded] = useState({});
 
@@ -30,8 +30,17 @@ export default function List( {list, addNodeToList} ) {
                         />
                     </span>
                 )}
+
+                {node?.id && (
+                    <span onClick={() => deleteNode(node.id)}>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRInOzZAX3_KiSl0piFlcVaHcmG5sAXw2cnCQ&s"
+                            alt="icon"
+                            className="icon"
+                        />
+                </span>
+                )}
                 {isExpanded?.[node.name] && node?.children && (
-                    <List list={node.children} addNodeToList={addNodeToList}/>)}
+                    <List list={node.children} addNodeToList={addNodeToList} deleteNode={deleteNode}/>)}
             </div>
             ))}
         </div>
